150bb05f70c96dfd91f583f705d5e5a4704b97a2
Neo.Li <neo.li@arcserve.com>
2018-08-27 11:11:27 +0800

    start gamana project

---
+++ gamana/src/main/java/com/jiayu/gamana/GamanaServiceApplication.java
@@ -0,0 +1,18 @@
+package com.jiayu.gamana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Service start on this point.
+ * @author Neo.Li
+ */
+@SpringBootApplication
+public class GamanaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamanaServiceApplication.class,args);
+	}
+
+}
